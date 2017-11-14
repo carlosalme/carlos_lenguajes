49d0924cd380741cce112f0dd562a45738634daa
T-102 <T-102@PC264>
2017-11-13 19:39:43 -0600

    Almeida Salmeron Carlos Eduardo clase Excepciones 13/11/17

---
+++ lenguajes/src/excepciones1/ExcepcionesTres.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones1;
+
+/**
+ *
+ * @author T-102
+ */
+public class ExcepcionesTres {
+    public static void main(String[] args) {
+        String x[]={"hola","mundo"};
+        System.out.println(x[2]);
+    }
+}
---
+++ lenguajes/src/excepciones1/ExcepcionesUno.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones1;
+
+/**
+ *
+ * @author T-102
+ */
+public class ExcepcionesUno {
+    public static void main(String[] args) {
+        //esta excepcion es muy conocida (excepcion aritmetica division entre 0)
+        int x=0;
+        float y=2;
+        System.out.println("Antes");
+        System.out.println(y/x);
+        System.out.println("Despues");
+    }
+}
